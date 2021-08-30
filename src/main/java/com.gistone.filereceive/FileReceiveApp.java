e0f7f02ea868352b231a096e1d0d30b98e9a7d0f
panzetao <panzetao@gistone.cn>
2021-08-30 14:03:43 +0800

    整洁代码

--- src/main/java/com.gistone.filereceive/FileReceiveApp.java
+++ src/main/java/com.gistone.filereceive/FileReceiveApp.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import sun.rmi.runtime.Log;
 
 import java.io.File;
 import java.io.IOException;
